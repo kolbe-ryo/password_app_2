3f4c83abfadaef01ebc4c2f174117e3b88af7ca3
kolbe <water.northground@gmail.com>
2022-05-25 23:11:09 +0900

    add pages

---
+++ lib/constants/bottom_navigation_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const bottomNavigationItems = [
+  BottomNavigationBarItem(
+    icon: Icon(Icons.access_time),
+    label: '登録順',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.apps_outlined),
+    label: 'ジャンル',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.settings_outlined),
+    label: '設定',
+  ),
+];
