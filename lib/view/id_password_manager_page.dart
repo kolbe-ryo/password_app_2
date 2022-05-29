ae75df0e7160394b5d6e42e666c9021dfc84a429
kolbe <water.northground@gmail.com>
2022-05-29 23:19:47 +0900

    add input text

--- lib/view/id_password_manager_page.dart
+++ lib/view/id_password_manager_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:password_app_2/constants/const_letter.dart';
+import 'package:password_app_2/constants/style.dart';
+import 'package:password_app_2/view/component/molecules/input_tile.dart';
 
 class IdPasswordManagerPage extends StatelessWidget {
   const IdPasswordManagerPage({Key? key}) : super(key: key);
@@ -7,9 +9,17 @@ class IdPasswordManagerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(kAppBarTitle),
+      appBar: AppBar(
+        title: const Text(kAppBarTitle),
+        iconTheme: const IconThemeData(color: kGreyColor),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: kSpacing),
+        itemBuilder: (context, index) => InputTile(
+          caption: '名前',
+          hintText: '登録名',
         ),
-        body: Text('Manager'));
+      ),
+    );
   }
 }
