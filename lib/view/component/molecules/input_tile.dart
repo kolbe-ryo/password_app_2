1b21df81bcb9ade24a0be68aaf4b00a4316f95f2
kolbe <water.northground@gmail.com>
2022-05-29 23:20:13 +0900

    change input text field

---
+++ lib/view/component/molecules/input_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputTile extends StatelessWidget {
+  const InputTile({
+    Key? key,
+    required this.caption,
+    required this.hintText,
+  }) : super(key: key);
+
+  final String caption;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(caption),
+        ),
+        TextField(
+          decoration: InputDecoration(hintText: hintText),
+        ),
+      ],
+    );
+  }
+}
