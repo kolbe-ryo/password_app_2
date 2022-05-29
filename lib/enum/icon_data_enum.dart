facd9fc9fe8fb487a4b77979e34a6b0cac2729e7
kolbe <water.northground@gmail.com>
2022-05-29 21:25:03 +0900

    icon enum

---
+++ lib/enum/icon_data_enum.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+enum IconDataEnum {
+  @JsonValue(0)
+  money,
+  @JsonValue(1)
+  shopping,
+  @JsonValue(2)
+  transportation,
+  @JsonValue(3)
+  sns,
+}
