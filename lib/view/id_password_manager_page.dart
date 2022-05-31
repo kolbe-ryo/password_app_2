3ffd9bd720ef32965540f838da8fb9383fbb004f
kolbe <water.northground@gmail.com>
2022-06-01 00:38:59 +0900

    error solve

--- lib/view/id_password_manager_page.dart
+++ lib/view/id_password_manager_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:password_app_2/constants/const_letter.dart';
 import 'package:password_app_2/constants/id_password_manager_item.dart';
 import 'package:password_app_2/constants/style.dart';
-import 'package:password_app_2/model/id_password_save_model.dart';
+import 'package:password_app_2/state/id_password_save_model.dart';
 import 'package:password_app_2/state/id_password_save_model_list.dart';
 import 'package:password_app_2/view/component/molecules/genre_select_tile.dart';
 import 'package:password_app_2/view/component/molecules/input_tile.dart';
