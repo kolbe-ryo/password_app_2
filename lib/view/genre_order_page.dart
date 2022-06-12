e8155f936667f7a74e9590423e30db057f1eeebb
kolbe <water.northground@gmail.com>
2022-06-12 21:11:34 +0900

    sorting by genre

--- lib/view/genre_order_page.dart
+++ lib/view/genre_order_page.dart
@@ -5,6 +5,7 @@ import 'package:password_app_2/enum/genre_data_enum.dart';
 import 'package:password_app_2/view/component/atom/nothing_data_text.dart';
 import 'package:password_app_2/view/component/atom/selected_icon_button.dart';
 import 'package:password_app_2/view/component/organisms/id_password_card.dart';
+import 'package:password_app_2/view/id_password_manager_page.dart';
 import 'package:password_app_2/view/registration_order_page.dart';
 
 class GenreOrderPage extends ConsumerWidget {
@@ -16,21 +17,28 @@ class GenreOrderPage extends ConsumerWidget {
       future: ref.read(savingProvider.notifier).get(),
       builder: ((context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          final idPasswordSaveModel =
-              ref.watch(savingProvider.select((model) => model.modelList));
-          return idPasswordSaveModel.isNotEmpty
-              ? Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: GenreDataEnum.values
-                          .map(
-                            (value) => SelectedIconButton(value),
-                          )
-                          .toList(),
-                    ),
-                    Expanded(
-                      child: GridView.builder(
+          final selectingGenre =
+              ref.watch(itemProvider.select((value) => value.genre));
+          final idPasswordSaveModel = ref.watch(
+            savingProvider.select(
+              (model) => model.modelList
+                  .where((element) => element.genre == selectingGenre)
+                  .toList(),
+            ),
+          );
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: GenreDataEnum.values
+                    .map(
+                      (value) => SelectedIconButton(value),
+                    )
+                    .toList(),
+              ),
+              Expanded(
+                child: idPasswordSaveModel.isNotEmpty
+                    ? GridView.builder(
                         gridDelegate:
                             const SliverGridDelegateWithFixedCrossAxisCount(
                           childAspectRatio: 1.3,
@@ -43,11 +51,11 @@ class GenreOrderPage extends ConsumerWidget {
                         },
                         itemCount: idPasswordSaveModel.length,
                         padding: const EdgeInsets.all(kSpacing),
-                      ),
-                    ),
-                  ],
-                )
-              : const NothingDataText();
+                      )
+                    : const NothingDataText(),
+              )
+            ],
+          );
         } else {
           return const Center(child: CircularProgressIndicator());
         }
