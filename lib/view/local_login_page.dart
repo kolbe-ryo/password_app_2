26dad5cc2c0946f25badf8e1a8f77c78438b97b1
kolbe <water.northground@gmail.com>
2022-06-14 00:42:03 +0900

    add passcode button

--- lib/view/local_login_page.dart
+++ lib/view/local_login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:password_app_2/constants/const_letter.dart';
+import 'package:password_app_2/constants/style.dart';
 import 'package:password_app_2/util/local_auth.dart';
 import 'package:password_app_2/view/life_cycle_detection_page.dart';
 
@@ -12,21 +13,68 @@ class LocalLoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(kAppBarTitle),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Login'),
-          onPressed: () async {
-            final isAuthenticated = await LocalAuth.authenticate();
-            if (isAuthenticated) {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: ((context) => const LifeCycleDetectionPage()),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.fingerprint,
+                    size: 50,
+                  ),
                 ),
-              );
-            }
-          },
-        ),
+                onPressed: () async => _push(context),
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                ),
+              ),
+              const SizedBox(height: kSpacing),
+              Text(
+                '生体認証',
+                style: kSecondTextStyle(),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.apps,
+                    size: 50,
+                  ),
+                ),
+                onPressed: () async => _push(context),
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                ),
+              ),
+              const SizedBox(height: kSpacing),
+              Text(
+                'パスコード',
+                style: kSecondTextStyle(),
+              ),
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> _push(BuildContext context) async {
+    final isAuthenticated = await LocalAuth.authenticate();
+    if (isAuthenticated) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: ((context) => const LifeCycleDetectionPage()),
+        ),
+      );
+    }
   }
 }
