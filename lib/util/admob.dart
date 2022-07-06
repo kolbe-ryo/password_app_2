e58fec1ed736a1ad1991db184d6a5aa96e59cb92
kolbe <water.northground@gmail.com>
2022-07-06 22:19:49 +0900

    admob for production

--- lib/util/admob.dart
+++ lib/util/admob.dart
@@ -9,15 +9,16 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class Admob {
   static String _getBannerAdUnitId() {
-    String? testBannerUnitId;
+    String? bannerUnitId;
     if (Platform.isAndroid) {
       // Android のとき
-      testBannerUnitId = "ca-app-pub-3940256099942544/6300978111"; // Androidのデモ用バナー広告ID
+      bannerUnitId = "ca-app-pub-3940256099942544/6300978111"; // Androidのデモ用バナー広告ID
     } else if (Platform.isIOS) {
       // iOSのとき
-      testBannerUnitId = "ca-app-pub-3940256099942544/2934735716"; // iOSのデモ用バナー広告ID
+      // testBannerUnitId = "ca-app-pub-3940256099942544/2934735716"; // iOSのデモ用バナー広告ID
+      bannerUnitId = "ca-app-pub-4793779857582845/1296487725";
     }
-    return testBannerUnitId ?? '';
+    return bannerUnitId ?? '';
   }
 
   static Future<void> _updateRequestConfig(bool isSimulator) async {
