6662006d8fb91b56e1abf91714c7da1494f50098
kolbe <water.northground@gmail.com>
2023-03-04 21:40:58 +0900

    change about app url

--- lib/constants/const_letter.dart
+++ lib/constants/const_letter.dart
@@ -4,7 +4,6 @@ const storageKey = 'idPassword';
 
 const passcordKey = 'passcode';
 
-const aboutAppUrl =
-    'https://peraichi.com/landing_pages/view/s8azs?_ga=2.234690526.1510206972.1657027608-546049198.1657027608';
+const aboutAppUrl = 'https://kolbe-app.site/';
 
 const isRelease = bool.fromEnvironment('dart.vm.product');
