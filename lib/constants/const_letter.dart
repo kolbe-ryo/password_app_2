db41a977522a4c8f85301fb44cee1af24d729663
kolbe <water.northground@gmail.com>
2024-01-06 16:13:39 +0900

    change about app url

--- lib/constants/const_letter.dart
+++ lib/constants/const_letter.dart
@@ -4,6 +4,6 @@ const storageKey = 'idPassword';
 
 const passcordKey = 'passcode';
 
-const aboutAppUrl = 'https://kolbe-app.site/';
+const aboutAppUrl = 'https://kolbe-app.tech/';
 
 const isRelease = bool.fromEnvironment('dart.vm.product');
